4b1dd077f5b431e84f74aa526ade1b12c301566c
m-sadkowski <msadkowski000@gmail.com>
2024-04-07 21:17:57 +0200

    aktualizacja 07.04.2024r
    84%

--- AISD-P2-V2.cpp
+++ AISD-P2-V2.cpp
@@ -91,7 +91,7 @@ public:
 	}
 };
 
-void input(Dynamic_array& tab, char &c) {
+void input(Dynamic_array& tab, char& c) {
 	tab.clear();
 	bool end = 0;
 	cin >> c >> c >> c;
@@ -111,7 +111,8 @@ void input(Dynamic_array& tab, char &c) {
 					break;
 				}
 				cin.putback(c);
-			} else if (num == 5) {
+			}
+			else if (num == 5) {
 				end = 1;
 				break;
 			}
@@ -122,19 +123,21 @@ void input(Dynamic_array& tab, char &c) {
 			if (c == 'b' || c == 'r') {
 				tab.push(c);
 				cin >> c;
-			} else if (c == '>') {
+			}
+			else if (c == '>') {
 				tab.push('-');
 			}
 		}
 	}
-} 
+}
 
 Pair PAWNS_NUMBER(const Dynamic_array& tab) {
 	Pair pawns;
 	for (int i = 0; i < tab.getSize(); i++) {
 		if (tab.get(i) == 'r') {
 			pawns.red++;
-		} else if (tab.get(i) == 'b') {
+		}
+		else if (tab.get(i) == 'b') {
 			pawns.blue++;
 		}
 	}
@@ -147,13 +150,14 @@ bool IS_BOARD_CORRECT(const Dynamic_array& tab) {
 	for (int i = 0; i < tab.getSize(); i++) {
 		if (tab.get(i) == 'r') {
 			red++;
-		} else if (tab.get(i) == 'b') {
+		}
+		else if (tab.get(i) == 'b') {
 			blue++;
 		}
 	}
 	if (red - blue == 0 || red - blue == 1) {
 		return true;
-	} 
+	}
 	return false;
 }
 
@@ -199,25 +203,27 @@ bool checkMap(int x, int y, char** arr, int size, bool** visited, char c) {
 	if (arr[x][y] == c) {
 		if (c == 'r' && y == size - 1) {
 			return true;
-		} else if (c == 'b' && x == size - 1) {
+		}
+		else if (c == 'b' && x == size - 1) {
 			return true;
 		}
+
 		if (y + 1 < size) if (arr[x][y + 1] == c && !visited[x][y + 1]) {
 			//cout << "z pola " << x << " " << y << " idziemy na " << x << " " << y + 1 << "\n";
 			if (checkMap(x, y + 1, arr, size, visited, c)) return true;
 		}
 		// +1 +1
-		if (y + 1 < size && x + 1 < size) if(arr[x + 1][y + 1] == c && !visited[x + 1][y + 1]) {
+		if (y + 1 < size && x + 1 < size) if (arr[x + 1][y + 1] == c && !visited[x + 1][y + 1]) {
 			//cout << "z pola " << x << " " << y << " idziemy na " << x + 1 << " " << y + 1 << "\n";
 			if (checkMap(x + 1, y + 1, arr, size, visited, c)) return true;
 		}
 		// -1 0
-		if (x > 0) if(arr[x - 1][y] == c && !visited[x - 1][y]) {
+		if (x > 0) if (arr[x - 1][y] == c && !visited[x - 1][y]) {
 			//cout << "z pola " << x << " " << y << " idziemy na " << x - 1 << " " << y << "\n";
 			if (checkMap(x - 1, y, arr, size, visited, c)) return true;
 		}
 		// + 1 0
-		if (x + 1 < size) if(arr[x + 1][y] == c && !visited[x + 1][y]) {
+		if (x + 1 < size) if (arr[x + 1][y] == c && !visited[x + 1][y]) {
 			//cout << "z pola " << x << " " << y << " idziemy na " << x + 1 << " " << y << "\n";
 			if (checkMap(x + 1, y, arr, size, visited, c)) return true;
 		}
@@ -233,30 +239,6 @@ bool checkMap(int x, int y, char** arr, int size, bool** visited, char c) {
 		}
 	}
 	return false;
-}
-
-void IS_BOARD_POSSIBLE(const Dynamic_array& tab) {
-	int size = BOARD_SIZE(tab);
-
-	if (!IS_BOARD_CORRECT(tab)) {
-		cout << "NO\n\n";
-		return;
-	}
-
-	char** arr = new char* [size];
-	for (int i = 0; i < size; i++) {
-		arr[i] = new char[size];
-	}
-
-	fillArray(arr, tab, size);
-	printArray(arr, size);
-
-	for (int i = 0; i < size; i++) {
-		delete[] arr[i];
-	}
-	delete[] arr;
-
-	return;
 }
 
 int IS_GAME_OVER(const Dynamic_array& tab) {
@@ -310,11 +292,59 @@ int IS_GAME_OVER(const Dynamic_array& tab) {
 	return 0;
 }
 
+void IS_BOARD_POSSIBLE(Dynamic_array tab) {
+
+	if (IS_GAME_OVER(tab) == 0 && IS_BOARD_CORRECT(tab)) {
+		cout << "YES\n\n";
+		return;
+	}
+	else if (!IS_BOARD_CORRECT(tab)) {
+		cout << "NO\n\n";
+		return;
+	}
+
+	int size = BOARD_SIZE(tab);
+	Pair pawns = PAWNS_NUMBER(tab);
+
+	if (IS_GAME_OVER(tab) == 1 && pawns.red - 1 == pawns.blue) {
+		for (int i = 0; i < size * size; i++) {
+			if (tab.get(i) == 'r') {
+				tab.set(i, '-');
+				if (IS_GAME_OVER(tab) != 1) {
+					cout << "YES\n\n";
+					tab.set(i, 'r');
+					return;
+				}
+				tab.set(i, 'r');
+			}
+		}
+		cout << "NO\n\n";
+		return;
+	}
+	else if (IS_GAME_OVER(tab) == 2 && pawns.blue == pawns.red) {
+		for (int i = 0; i < size * size; i++) {
+			if (tab.get(i) == 'b') {
+				tab.set(i, '-');
+				if (IS_GAME_OVER(tab) != 2) {
+					cout << "YES\n\n";
+					tab.set(i, 'b');
+					return;
+				}
+				tab.set(i, 'b');
+			}
+		}
+		cout << "NO\n\n";
+		return;
+	}
+
+	cout << "NO\n\n";
+	return;
+}
+
 void CAN_WIN_NAIVE_OPPONENT(Dynamic_array tab)
 {
 	int size = BOARD_SIZE(tab);
-	if (IS_GAME_OVER(tab) || !IS_BOARD_CORRECT(tab))
-	{
+	if (IS_GAME_OVER(tab) || !IS_BOARD_CORRECT(tab)) {
 		cout << "NO\nNO\nNO\nNO\n";
 		return;
 	}
@@ -379,16 +409,17 @@ void CAN_WIN_NAIVE_OPPONENT(Dynamic_array tab)
 		for (int i = 0; i < size * size; i++) {
 			if (tab.get(i) == '-') {
 				tab.set(i, 'r');
-				if(!IS_GAME_OVER(tab))
-				for (int j = 0; j < size * size; j++) {
-					if (tab.get(j) == '-') {
-						tab.set(j, 'r');
-						if (IS_GAME_OVER(tab) == 1) {
-							win = true;
+				if (!IS_GAME_OVER(tab)) {
+					for (int j = 0; j < size * size; j++) {
+						if (tab.get(j) == '-') {
+							tab.set(j, 'r');
+							if (IS_GAME_OVER(tab) == 1) {
+								win = true;
+								tab.set(j, '-');
+								break;
+							}
 							tab.set(j, '-');
-							break;
 						}
-						tab.set(j, '-');
 					}
 				}
 				tab.set(i, '-');
@@ -409,16 +440,17 @@ void CAN_WIN_NAIVE_OPPONENT(Dynamic_array tab)
 		for (int i = 0; i < size * size; i++) {
 			if (tab.get(i) == '-') {
 				tab.set(i, 'b');
-				if (!IS_GAME_OVER(tab))
-				for (int j = 0; j < size * size; j++) {
-					if (tab.get(j) == '-') {
-						tab.set(j, 'b');
-						if (IS_GAME_OVER(tab) == 2) {
-							win = true;
+				if (!IS_GAME_OVER(tab)) {
+					for (int j = 0; j < size * size; j++) {
+						if (tab.get(j) == '-') {
+							tab.set(j, 'b');
+							if (IS_GAME_OVER(tab) == 2) {
+								win = true;
+								tab.set(j, '-');
+								break;
+							}
 							tab.set(j, '-');
-							break;
 						}
-						tab.set(j, '-');
 					}
 				}
 				tab.set(i, '-');
@@ -433,7 +465,7 @@ void CAN_WIN_NAIVE_OPPONENT(Dynamic_array tab)
 	else {
 		cout << "NO\n";
 	}
-	
+
 	for (int i = 0; i < size; i++) {
 		delete[] arr[i];
 	}
@@ -453,46 +485,55 @@ int main()
 			function = "";
 			cin.putback(c);
 			input(tab, c);
-		} else {
+		}
+		else {
 			getline(cin, function);
-		} 
-		if (function == "OARD_SIZE") {
+		}
+
+		if (function == "OARD_SIZE") { // READY
 			// 8 %
 			cout << BOARD_SIZE(tab) << "\n\n";
-		} else if (function == "AWNS_NUMBER") {
+		}
+		else if (function == "AWNS_NUMBER") { // READY
 			// 8 %
 			Pair pawns = PAWNS_NUMBER(tab);
 			cout << pawns.red + pawns.blue << "\n\n";
-		} else if (function == "S_BOARD_CORRECT") {
+		}
+		else if (function == "S_BOARD_CORRECT") { // READY
 			// 8 %
 			if (IS_BOARD_CORRECT(tab)) {
 				cout << "YES\n\n";
-			} else {
+			}
+			else {
 				cout << "NO\n\n";
 			}
-		} else if (function == "S_GAME_OVER") {
+		}
+		else if (function == "S_GAME_OVER") { // READY
 			// 20 %
 			if (IS_BOARD_CORRECT(tab)) {
-				int x = IS_GAME_OVER(tab);
-				if (x == 0) cout << "NO\n\n";
-				else if (x == 1) cout << "YES RED\n\n";
-				else if (x == 2) cout << "YES BLUE\n\n";
-			} else {
+				int over = IS_GAME_OVER(tab);
+				if (over == 0) cout << "NO\n\n";
+				else if (over == 1) cout << "YES RED\n\n";
+				else if (over == 2) cout << "YES BLUE\n\n";
+			}
+			else {
 				cout << "NO\n\n";
 			}
-		} else if (function == "S_BOARD_POSSIBLE") {
+		}
+		else if (function == "S_BOARD_POSSIBLE") { // READY
 			// 20 %
 			IS_BOARD_POSSIBLE(tab);
-			break;
-		} else if (function == "AN_RED_WIN_IN_1_MOVE_WITH_NAIVE_OPPONENT") {
+		}
+		else if (function == "AN_RED_WIN_IN_1_MOVE_WITH_NAIVE_OPPONENT") { // READY
 			// 20 %
 			getline(cin, function);
 			getline(cin, function);
 			getline(cin, function);
 			CAN_WIN_NAIVE_OPPONENT(tab);
 			cout << "\n\n";
-		} else if (function == "AN_RED_WIN_IN_1_MOVE_WITH_PERFECT_OPPONENT") {
-			// 20 %
+		}
+		else if (function == "AN_RED_WIN_IN_1_MOVE_WITH_PERFECT_OPPONENT") {
+			// 16 %
 			getline(cin, function);
 			getline(cin, function);
 			getline(cin, function);
